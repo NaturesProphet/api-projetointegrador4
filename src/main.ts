b0fc60cab81353ffa542d71281bb95aa18da8e19
Mateus Garcia <mateus.gigainfo@gmail.com>
2019-06-20 00:58:44 -0300

    feat(Cors): cors enabled

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ import { port } from './common/api.conf';
 
 async function bootstrap () {
   const app = await NestFactory.create( AppModule );
+  app.enableCors();
   await app.listen( port );
 }
 bootstrap();
